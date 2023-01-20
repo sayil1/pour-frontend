eab020b0a3d470417c811a24c660b746b3db353c
sayil1 <sundaysayil4u@gmail.com>
2023-01-20 16:01:04 +0100

    use online http

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,5 +1,7 @@
 export const environment = {
-    baseUrlServiceService: 'https://employments-data.vercel.app',
+    // baseUrlServiceService: 'https://employments-data.vercel.app',
+    baseUrlServiceService: 'http://104.238.184.127/api/',
+
 
     // baseUrlServiceService:'http://localhost:4000'
 
