ecdb7085c8dce5cd397ef1153d2e7ce7b33976c9
sayil1 <sundaysayil4u@gmail.com>
2023-01-20 09:20:43 +0100

    edit environment

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    // baseUrlServiceService: 'https://employments-data.vercel.app',
+    baseUrlServiceService: 'https://employments-data.vercel.app',
 
-    baseUrlServiceService:'http://localhost:4000'
+    // baseUrlServiceService:'http://localhost:4000'
 
 }
